d791817e5505c5252309ba7901f7f2519aedd230
Michael Orr <michael@orr.co>
2014-11-18 14:51:16 -0500

    adding an option for github enterprise urls

--- test_helga_pull_requests.py
+++ test_helga_pull_requests.py
@@ -27,6 +27,8 @@ def test_returns_none_with_no_matches():
 @patch('helga_pull_requests.settings')
 def test_ignores_match_without_default(settings):
     settings.PULL_REQUESTS_DEFAULT_ACCOUNT = ''
+    del settings.PULL_REQUESTS_GITHUB_HOST
+
     resp = pull_requests('', '', 'me', '', [
         ('', '', 'bar', '10'),
         ('', 'baz', 'qux', '42'),
@@ -37,6 +39,8 @@ def test_ignores_match_without_default(settings):
 @patch('helga_pull_requests.settings')
 def test_uses_default_account(settings):
     settings.PULL_REQUESTS_DEFAULT_ACCOUNT = 'foo'
+    del settings.PULL_REQUESTS_GITHUB_HOST
+
     resp = pull_requests('', '', 'me', '', [
         ('', '', 'bar', '10'),
         ('', 'baz', 'qux', '42'),
@@ -44,6 +48,18 @@ def test_uses_default_account(settings):
     assert resp == ('me might be talking about pull request: '
                     'https://github.com/foo/bar/pull/10, '
                     'https://github.com/baz/qux/pull/42')
+
+
+@patch('helga_pull_requests.settings')
+def test_uses_nondefault_github_url(settings):
+    settings.PULL_REQUESTS_DEFAULT_ACCOUNT = ''
+    settings.PULL_REQUESTS_GITHUB_HOST = 'http://my_github.com'
+
+    resp = pull_requests('', '', 'me', '', [
+        ('', 'foo', 'bar', '10'),
+    ])
+    assert resp == ('me might be talking about pull request: '
+                    'http://my_github.com/foo/bar/pull/10')
 
 
 def test_match_regex():
